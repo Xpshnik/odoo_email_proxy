b37ac4681fa84afde37132f65fcd444e8b952685
Viktor Fedoriv <viktor.fedoriv@smartteksas.com>
2022-12-29 14:56:25 +0200

    fix incorrect hook logic

--- hooks.py
+++ hooks.py
@@ -1,23 +1,25 @@
+from psycopg2 import sql
 from odoo.api import Environment, SUPERUSER_ID
 
 
 def post_init_hook(cr, registry):
     env = Environment(cr, SUPERUSER_ID, {})
-    models_with_emails = env['email.proxy.mixin']._MODEL_TO_EMAIL_FIELD_MAPPING
+    models_with_emails = env['res.email.mixin']._MODEL_TO_EMAIL_FIELD_MAPPING
     for model, email_field in models_with_emails.items():
         cr.execute(
-            '''
+            sql.SQL('''
                 INSERT INTO res_email (email, res_id, res_model,
                 create_date, write_date, create_uid, write_uid)
-                SELECT %(email_field)s, id, '%(model)s',
+                SELECT {email_field}, id, %(model)s,
                 current_timestamp, current_timestamp, create_uid, write_uid
-                FROM %(table)s
-                WHERE %(email_field)s IS NOT NULL;
-            ''',
+                FROM {table}
+                WHERE {email_field} IS NOT NULL;
+            ''').format(
+                email_field=sql.Identifier(email_field),
+                table=sql.Identifier(model.replace('.', '_')),
+            ),
             {
                 'model': model,
-                'email_field': email_field,
-                'table': model.replace('.', '_')
             },
         )
 
